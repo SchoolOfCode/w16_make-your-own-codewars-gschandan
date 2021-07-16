83678fd90e66952d66e2e4565be8d7f05f47aa69
gschandan <gsc128@gmail.com>
2021-07-16 23:01:24 +0000

    added tests, mildly hidden solution

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
 /* 
-👉 Write your kata here!
+You are walking to the local coffee shop, entering at the north west corner of a
+ square in the city centre, when you see a doughnut, lying abandoned on the 
+ floor, sprinkles scattered haphazardly around. Senses tingling and adrenaline 
+ surging, you notice a traces of jam and powdered sugar scattered around the 
+ area, suggesting signs of a struggle. You are intruiged and decide to 
+ investigate further, when it suddenly flips onto it's side and rolls away. You 
+ chase after the magical doughnut, whilst trying to reassure it you mean it no 
+ harm. Luckily it is early in the morning, and no-one is around to witness your 
+ seeming loss of grip with reality.
+
+Whilst chasing the doughnut, you realise it is moving in a peculiar pattern. 
+
+It only moves directly south, or directly east, by one paving slab at a time, 
+and it appears to be coincidentally heading towards the same local coffee shop, 
+the building directly south-east from you.
+
+In your efforts to capture the fleeing doughnut, you try to predict how many 
+different paths it can take to reach the shop. This will depend on the size of 
+the square, which can be rectangular, given as (x,y)
+--------------------------------------------------------------------------------
+Given a grid size, (x,y), calculate the unique possible paths that can be taken 
+whilst chasing the doughnut, only moving south and east. Return the number of 
+paths as an integer. If the input does not contain two positive integers, return
+ an error "Invalid Input!"
+--------------------------------------------------------------------------------
+Performance: Ideally better than O(2^n+m) - uncomment the advanced tests to test 
+the performance of your solution - aim for O(n*m)
+
 */
 
-//👉 Write the function your CodeWarriors will start with below here:
+const doughnutChaser = (x,y) =>{
+
+}
+
+module.exports = {
+    doughnutChaser
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,52 @@
-//👉 Write your tests below here:
+const {
+    doughnutChaser
+} = require("./main")
+//Unfortunately I can't include random tests without showing the solution
+
+describe('Fixed tests - Input Validation', () => {
+    test("Given an invalid input, it should throw the specified error",()=>{
+        expect(() => doughnutChaser(null,null)).toThrow("Invalid Input!")
+        expect(() => doughnutChaser(NaN,5)).toThrow("Invalid Input!")
+        expect(() => doughnutChaser(undefined,".")).toThrow("Invalid Input!")
+        expect(() => doughnutChaser("5",true)).toThrow("Invalid Input!")
+        expect(() => doughnutChaser(-5,2)).toThrow("Invalid Input!")
+        expect(() => doughnutChaser(2.2,Math.PI)).toThrow("Invalid Input!")
+    })
+})
+
+describe('Fixed tests - Small sized grids', () => {
+    test("Given a valid input, return the number of unique paths",()=>{
+        expect(doughnutChaser(1,1)).toBe(1);
+        expect(doughnutChaser(0,1)).toBe(0);
+        expect(doughnutChaser(0,0)).not.toBe(1);
+        expect(doughnutChaser(2,2)).toBe(2);
+        expect(doughnutChaser(3,3)).toBe(6);
+        expect(doughnutChaser(3,8)).toBe(36);
+        expect(doughnutChaser(2,9)).toBe(9);
+        expect(doughnutChaser(3,5)).toBe(15);
+    })
+})
+//Uncomment these one at a time only when you have optimised your soltuion
+//or else it might freeze whilst calculating
+
+// describe('Fixed tests - Increasing grid sizes', () => {
+    // test("Given a grid of (23,4), return the number of unique paths",()=>{
+    //     expect(doughnutChaser(23,4)).toBe(2300);
+    // })
+    // test("Given a grid of (5,55), return the number of unique paths",()=>{
+    //     expect(doughnutChaser(5,55)).toBe(424270);
+    // })
+    // test("Given a grid of (14,22), return the number of unique paths",()=>{
+    //     expect(doughnutChaser(14,22)).toBe(927983760);
+    // })
+    // test("Given a grid of (22,21, return the number of unique paths",()=>{
+    //     expect(doughnutChaser(22,21)).toBe(269128937220);
+    // })
+    // test("Given a grid of (35,22), return the number of unique paths",()=>{
+    //     expect(doughnutChaser(35,22)).toBe(841728816603675);
+    // })
+// })
+
+
+
+
